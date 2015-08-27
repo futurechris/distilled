9e6190c5589b74ac61ba8fc8e2739d4b79e18ea7
Chris Mills-Price <millspch@gmail.com>
2015-08-26 22:38:34 -0700

    Create puzzle MVC skeleton

---
+++ Assets/Scripts/PuzzleRenderer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PuzzleRenderer : MonoBehaviour {
+
+	public GameObject tileParent;
+
+	private LogicPuzzle myPuzzle;
+
+
+	////////////////////////////////////////
+	#region MonoBehaviours methods & helpers
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	#endregion MonoBehaviours methods & helpers
+	////////////////////////////////////////
+
+	////////////////////////////////////////
+	#region Getters & Setters
+	
+	public void setPuzzle(LogicPuzzle newPuzzle)
+	{
+		renderCleanup();
+		myPuzzle = newPuzzle;
+		instantiateRendering();
+		updateRender();
+	}
+	
+	#endregion Getters & Setters
+	////////////////////////////////////////
+
+	////////////////////////////////////////
+	#region Render helpers
+
+	// Any cleanup we might need to do if we replace existing
+	private void renderCleanup()
+	{
+
+	}
+	
+	// Any object creation
+	private void instantiateRendering()
+	{
+		
+	}
+	
+	// Update all the values from the existing myTile setup
+	private void updateRender()
+	{
+		
+	}
+
+	#endregion Render helpers
+	////////////////////////////////////////
+
+
+}
---
+++ Assets/Scripts/TileRenderer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TileRenderer : MonoBehaviour {
+
+	public Image myImage;
+
+	private LogicTile myTile;
+	
+	////////////////////////////////////////
+	#region MonoBehaviour Methods & Helpers
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	#endregion MonoBehaviour Methods & Helpers
+	////////////////////////////////////////
+
+	////////////////////////////////////////
+	#region Getters & Setters
+
+	public void setTile(LogicTile newTile)
+	{
+		renderCleanup();
+		myTile = newTile;
+		instantiateRendering();
+		updateRender();
+	}
+
+	#endregion Getters & Setters
+	////////////////////////////////////////
+	
+	////////////////////////////////////////
+	#region Render Helpers
+
+	// Any cleanup we might need to do if we replace existing
+	private void renderCleanup(){}
+
+	// Any object creation
+	private void instantiateRendering()
+	{
+
+	}
+
+	// Update all the values from the existing myTile setup
+	private void updateRender()
+	{
+
+	}
+
+	#endregion Render Helpers
+	////////////////////////////////////////
+}
